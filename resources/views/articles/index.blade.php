0e70c0674bdbec6fa2ceed154c774b750ee6f8d1
Lilian Levano <zey.lilianlevano@gmail.com>
2026-04-30 01:27:09 +0200

    bugfix

--- resources/views/articles/index.blade.php
+++ resources/views/articles/index.blade.php
@@ -4,18 +4,14 @@
 
     <div class="flex flex-row  flex-wrap justify-center items-center gap-20">
         @foreach($articles as $article)
-<<<<<<< HEAD
-            <a href="/articles/{{$article->id}}" class="basis-2xl">
-                <span class="bg-gray-300 rounded-2xl pl-4 pr-4">{{$article->category->name}}</span>
-                <span class="text-right text-green-400">By: {{$article->author->name}}</span>
-                <h2 class="text-2xl font-bold ">{{$article->title}}</h2>
-=======
+
+
+
             <div class="basis-2xl">
 
 
-                <span class="bg-gray-300 rounded-2xl pl-4 pr-4">{{$article->category}}</span>
+                <span class="bg-gray-300 rounded-2xl pl-4 pr-4">{{$article->category->name}}</span>
                 <a href="/articles/{{$article->id}}">
->>>>>>> 7be68f648d65bcb8195beca8d495042e0e12a919
 
                     <h2 class="text-2xl font-bold">{{$article->title}}</h2>
                     <p>{{$article->content}}</p>
