7c85236e5293a7287e87af61c76c7e28fd224c97
Lilian Levano <zey.lilianlevano@gmail.com>
2026-04-30 01:50:32 +0200

    add syle in index articles

--- resources/views/articles/index.blade.php
+++ resources/views/articles/index.blade.php
@@ -7,20 +7,23 @@
 
 
 
-            <div class="basis-2xl">
+            <div class="flex flex-col basis-2xl border justify-center rounded-3xl border-b-black p-4 h-56">
 
 
-                <span class="bg-gray-300 rounded-2xl pl-4 pr-4">{{$article->category->name}}</span>
+                <span class="bg-gray-300 rounded-2xl pl-4 pr-4 max-w-fit">{{$article->category->name}}</span>
                 <a href="/articles/{{$article->id}}">
 
                     <h2 class="text-2xl font-bold">{{$article->title}}</h2>
                     <p>{{$article->content}}</p>
                 </a>
 
-                <b>By</b>:
-                <a href="/users/{{$article->author->id}}" class="text-blue-500 underline">
-                    {{$article->author->name}}
-                </a>
+                <div class="flex gap-1">
+                    <b>By:</b>
+                    <a href="/users/{{$article->author->id}}" class="text-blue-500 underline">
+                        {{$article->author->name}}
+                    </a>
+                </div>
+
             </div>
         @endforeach
     </div>
