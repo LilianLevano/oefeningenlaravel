94b11aaa5f06db6256ab428fe6ca4868fce644a0
Lilian Levano <zey.lilianlevano@gmail.com>
2026-04-29 13:32:59 +0200

    add task model, migration, factory

--- resources/views/task.blade.php
+++ resources/views/task.blade.php
@@ -1,21 +1,25 @@
 <x-site-layout>
-<div>
+
+    <h1 class="text-3xl font-bold text-center mb-10">Task Manager</h1>
+
+<div class="flex flex-col gap-8">
     @foreach($tasks as $task)
 
-        <h3 class="text-2xl font-bold">{{$task['title']}}</h3>
-        <p>{{$task['description']}}</p>
-        <p>Deadline: {{$task['due_date']}}</p>
-        <p>Status:
-        @if($task['completed'] == 0)
+        <div>
+            <h3 class="text-2xl font-bold">{{$task['title']}}</h3>
+            <p>{{$task['description']}}</p>
+            <p>Deadline: {{$task['due_date']}}</p>
+            <p>Status:
+                @if($task['completed'] == 0)
+                    <span>❌</span>
+                @else
+                    <span>✔️</span>
+                @endif
+            </p>
 
-            <span>❌</span>
+            <p>Prioriteit: {{$task['priority']}}</p>
+        </div>
 
-        @else
-            <span>✔️</span>
-        @endif
-        </p>
-
-        <p>Prioriteit: {{$task['priority']}}</p>
 
 
     @endforeach
