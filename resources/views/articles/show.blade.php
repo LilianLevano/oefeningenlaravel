093ae55b97833a7de05e899370ad84298065154d
Lilian Levano <zey.lilianlevano@gmail.com>
2026-04-30 01:31:20 +0200

    bugfix

--- resources/views/articles/show.blade.php
+++ resources/views/articles/show.blade.php
@@ -2,19 +2,20 @@
     @slot('title', $article->title)
 
     <div class="basis-2xl">
-<<<<<<< HEAD
+
+
+
+
         <span class="bg-gray-300 rounded-2xl pl-4 pr-4">{{$article->category->name}}</span>
-        <span class="text-right text-green-400">{{$article->author->name}}</span>
-=======
-        <span class="bg-gray-300 rounded-2xl pl-4 pr-4">{{$article->category}}</span>
-        <span class="text-right text-green-400">
-            <a href="/users/{{$article->author->id}}" class="text-blue-500 underline">
-                    {{$article->author->name}}
-                </a></span>
->>>>>>> 7be68f648d65bcb8195beca8d495042e0e12a919
-        <h2 class="text-2xl font-bold ">{{$article->title}}</h2>
+        <a href="/articles/{{$article->id}}">
 
+            <h2 class="text-2xl font-bold">{{$article->title}}</h2>
+            <p>{{$article->content}}</p>
+        </a>
 
-        <p>{{$article->content}}</p>
+        <b>By</b>:
+        <a href="/users/{{$article->author->id}}" class="text-blue-500 underline">
+            {{$article->author->name}}
+        </a>
     </div>
 </x-site-layout>
